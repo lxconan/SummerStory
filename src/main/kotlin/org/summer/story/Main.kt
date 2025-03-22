3dac90b5d9ad924497520cc6c67409e6c744e086
Liu Xia <lxconan@163.com>
2025-03-22 20:07:01 +0800

    feat: add method to disable JUL logging from 3rd party libraries.

--- src/main/kotlin/org/summer/story/Main.kt
+++ src/main/kotlin/org/summer/story/Main.kt
@@ -3,9 +3,18 @@ package org.summer.story
 import org.summer.story.server.GlobalService
 
 fun main() {
+    disableJulLogging()
+
     Runtime.getRuntime().addShutdownHook(Thread {
         GlobalService.stop()
     })
 
     GlobalService.start()
+}
+
+fun disableJulLogging() {
+    val rootLogger = java.util.logging.Logger.getLogger("")
+    rootLogger.handlers
+        .filterIsInstance<java.util.logging.ConsoleHandler>()
+        .forEach(rootLogger::removeHandler)
 }
