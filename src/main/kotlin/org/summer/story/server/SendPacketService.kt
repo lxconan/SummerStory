bd174bc4bcc05e04e33eb17833f6b88892368213
Liu Xia <lxconan@163.com>
2025-03-25 08:49:56 +0800

    feat: add debug logging for packet sending in SendPacketServiceImpl

--- src/main/kotlin/org/summer/story/server/SendPacketService.kt
+++ src/main/kotlin/org/summer/story/server/SendPacketService.kt
@@ -4,18 +4,22 @@ import io.netty.channel.Channel
 import io.netty.channel.ChannelFuture
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.summer.story.config.GlobalConfiguration
 import org.summer.story.server.dtos.OutDto
 
 interface SendPacketService {
     fun sendPacket(channel: Channel, dto: OutDto): ChannelFuture
 }
 
-class SendPacketServiceImpl : SendPacketService {
+class SendPacketServiceImpl(private val configuration: GlobalConfiguration) : SendPacketService {
     companion object {
         private val logger: Logger = LoggerFactory.getLogger(SendPacketServiceImpl::class.java)
     }
 
     override fun sendPacket(channel: Channel, dto: OutDto): ChannelFuture {
+        if (configuration.debug.recordPacket) {
+            logger.info("Sending packet: {}", dto.toString())
+        }
         return channel.writeAndFlush(dto.toPacket())
     }
 }
