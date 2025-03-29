dd6c32fece0774a3f9391c0c7b532086234d9fc7
Liu Xia <lxconan@163.com>
2025-03-29 21:55:42 +0800

    doc: explain why we implement sha256 this way.

--- src/main/kotlin/org/summer/story/net/encryption/HashAlgorithm.kt
+++ src/main/kotlin/org/summer/story/net/encryption/HashAlgorithm.kt
@@ -5,6 +5,8 @@ import org.summer.story.net.packet.HexFormater
 class HashAlgorithm {
     fun sha256(input: String): String {
         val bytes = input.toByteArray(charset = Charsets.UTF_8)
+        // We get instance of SHA-256 algorithm every time instead of storing it in a field because it is not
+        // thread-safe. This is fast enough for our use case.
         val digest: ByteArray = java.security.MessageDigest.getInstance("SHA-256").digest(bytes)
         return HexFormater.toCompactHexString(digest)
     }
