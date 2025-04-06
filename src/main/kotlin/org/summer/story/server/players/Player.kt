434ca0e843c15532901b50b973351025c51e613c
Liu Xia <lxconan@163.com>
2025-04-06 21:27:47 +0800

    feat: add thread-safe closing handling for Player class

--- src/main/kotlin/org/summer/story/server/players/Player.kt
+++ src/main/kotlin/org/summer/story/server/players/Player.kt
@@ -6,6 +6,7 @@ import org.summer.story.server.NetworkContext
 import org.summer.story.server.SendPacketService
 import org.summer.story.server.TimeService
 import org.summer.story.server.dtos.OutDto
+import java.util.concurrent.atomic.AtomicBoolean
 
 // Please note that the player can be accessed from netty's event loop thread and from player's thread. So all the
 // methods in the implementation should be thread-safe.
@@ -36,12 +37,18 @@ class PlayerImpl(
     override val clientIp: String
     override var accountContext: AccountContext? = null
 
+    private val _isClosed: AtomicBoolean = AtomicBoolean(false)
+
     init {
         require(networkContext.isValid()) { "Network context is invalid" }
         clientIp = networkContext.clientIp!!
     }
 
     override fun updateChannel(channel: Channel) {
+        if (_isClosed.get()) {
+            throw IllegalStateException("Player is closed, cannot update channel")
+        }
+
         synchronized(this) {
             _ioChannel = channel
         }
@@ -59,10 +66,19 @@ class PlayerImpl(
         // The Channel.close() method in Netty is thread-safe. Netty ensures that the close() method can be safely
         // called from any thread, and it will handle the necessary synchronization internally. This means you do
         // not need to synchronize the call to close() yourself.
+        if (!_isClosed.compareAndSet(false, true)) {
+            logger.warn("Player is already closed, cannot close again.")
+            return
+        }
         synchronized(this) { _ioChannel }?.close()
     }
 
     override fun respond(dto: OutDto) {
+        if (_isClosed.get()) {
+            logger.warn("Player is closed, cannot send packet: {}", dto.toString())
+            return
+        }
+
         val theChannel: Channel? = safelyGetChannel()
         if (theChannel == null) {
             logger.warn("Channel is null or inactive, cannot send packet: {}", dto.toString())
@@ -75,6 +91,8 @@ class PlayerImpl(
     }
 
     private fun safelyGetChannel(): Channel? {
+        if (_isClosed.get()) { return null }
+
         val theChannel: Channel? = synchronized(this) { _ioChannel }
 
         // The Channel.isActive method in Netty is thread-safe. It can be safely called from any thread without
